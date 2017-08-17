ec6bac9421dfe65604525a4e2bb937be95ba1044
AvidRP <s2pant@edu.uwaterloo.ca>
2017-08-16 22:37:59 -0600

    toggle class for popup

--- public/js/awards.js
+++ public/js/awards.js
@@ -1,1 +1,6 @@
-
+// $(document).ready(function(){
+//   // $(".popup").hide();
+//   $(".moreInfoButton").click(function(){
+//       $(this).parents(".container").find(".popup").toggle("slow");
+//   });
+// });
